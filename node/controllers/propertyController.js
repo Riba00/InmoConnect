36be500fb42b80dad869e56085ed02a6179c2bc7
Riba00 <jordi.riba.ferre@gmail.com>
2024-05-31 13:38:07 +0200

    delete property and images

--- node/controllers/propertyController.js
+++ node/controllers/propertyController.js
@@ -1,3 +1,4 @@
+import { unlink } from "node:fs/promises";
 import { validationResult } from "express-validator";
 import { Category, Price, Property } from "../models/index.js";
 
@@ -9,15 +10,15 @@ const admin = async (req, res) => {
       userId: id,
     },
     include: [
-        {
-            model: Category,
-            as: "category",
-        },
-        {
-            model: Price,
-            as: "price",
-        },
-    ]
+      {
+        model: Category,
+        as: "category",
+      },
+      {
+        model: Price,
+        as: "price",
+      },
+    ],
   });
 
   properties.forEach((property) => {
@@ -28,6 +29,7 @@ const admin = async (req, res) => {
 
   res.render("properties/admin", {
     page: "My Properties",
+    csrfToken: req.csrfToken(),
     properties,
   });
 };
@@ -57,7 +59,7 @@ const store = async (req, res) => {
       Price.findAll(),
     ]);
 
-    res.render("properties/create", {
+    return res.render("properties/create", {
       page: "Create Property",
       csrfToken: req.csrfToken(),
       categories,
@@ -167,4 +169,134 @@ const storeImage = async (req, res, next) => {
   }
 };
 
-export { admin, create, store, addImage, storeImage };
+const edit = async (req, res) => {
+  const { id } = req.params;
+
+  const property = await Property.findByPk(id);
+
+  if (!property) {
+    return res.redirect("/my-properties");
+  }
+
+  if (property.userId.toString() !== req.user.id.toString()) {
+    return res.redirect("/my-properties");
+  }
+
+  const [categories, prices] = await Promise.all([
+    Category.findAll(),
+    Price.findAll(),
+  ]);
+
+  res.render("properties/edit", {
+    page: `Edit Property: ${property.title}`,
+    csrfToken: req.csrfToken(),
+    categories,
+    prices,
+    data: property,
+  });
+};
+
+const update = async (req, res) => {
+  let result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    const [categories, prices] = await Promise.all([
+      Category.findAll(),
+      Price.findAll(),
+    ]);
+
+    return res.render("properties/edit", {
+      page: "Edit Property",
+      csrfToken: req.csrfToken(),
+      categories,
+      prices,
+      errors: result.array(),
+      data: req.body,
+    });
+  }
+
+  const { id } = req.params;
+
+  const property = await Property.findByPk(id);
+
+  if (!property) {
+    return res.redirect("/my-properties");
+  }
+
+  if (property.userId.toString() !== req.user.id.toString()) {
+    return res.redirect("/my-properties");
+  }
+
+  try {
+    const {
+      title,
+      description,
+      category,
+      price,
+      rooms,
+      wcs,
+      street,
+      parkings,
+      lat,
+      lng,
+      price: priceId,
+      category: categoryId,
+    } = req.body;
+
+    property.set({
+      title,
+      description,
+      rooms,
+      wcs,
+      street,
+      parkings,
+      lat,
+      lng,
+      priceId,
+      categoryId,
+    });
+
+    await property.save();
+
+    res.redirect("/my-properties");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteProperty = async (req, res) => {
+  const { id } = req.params;
+
+  const property = await Property.findByPk(id);
+
+  if (!property) {
+    return res.redirect("/my-properties");
+  }
+
+  if (property.userId.toString() !== req.user.id.toString()) {
+    return res.redirect("/my-properties");
+  }
+
+  try {
+    JSON.parse(property.images).forEach(async (image) => {
+      await unlink(`public/uploads/${image}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  await property.destroy();
+
+  res.redirect("/my-properties");
+};
+
+export {
+  admin,
+  create,
+  store,
+  addImage,
+  storeImage,
+  edit,
+  update,
+  deleteProperty,
+};
--- node/routes/propertiesRoutes.js
+++ node/routes/propertiesRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { body } from 'express-validator'
-import { addImage, admin, create, store, storeImage } from '../controllers/propertyController.js'
+import { addImage, admin, create, store, storeImage, edit, update, deleteProperty } from '../controllers/propertyController.js'
 import protectRoute from "../middleware/protectRoute.js"
 import upload from "../middleware/uploadImage.js"
 
@@ -31,5 +31,23 @@ router.post('/properties/add-image/:id',
     upload.any(),
     storeImage
 )
+
+router.get('/properties/edit/:id', protectRoute, edit)
+
+router.post('/properties/edit/:id', protectRoute,
+    body('title').notEmpty().withMessage('Title is required'),
+    body('description')
+        .notEmpty().withMessage('Description is required')
+        .isLength({ max:200 }).withMessage('Description is too long'),
+    body('category').isNumeric().withMessage('Category is required'),
+    body('price').isNumeric().withMessage('Price is required'),
+    body('rooms').isNumeric().withMessage('Rooms is required'),
+    body('wcs').isNumeric().withMessage('WCs is required'),
+    body('parkings').isNumeric().withMessage('Parking is required'),
+    body('lat').notEmpty().withMessage('Property location is required'),
+    update
+)
+
+router.post('/properties/deleteProperty/:id', protectRoute, deleteProperty)
 
 export default router
