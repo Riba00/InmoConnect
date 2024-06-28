08ca40e1605db115ea5e7b3add1c0a9f79f140b9
Riba00 <jordi.riba.ferre@gmail.com>
2024-06-28 20:16:08 +0200

    chore: Update appController to fetch categories and prices for initialization

--- node/controllers/appController.js
+++ node/controllers/appController.js
@@ -1,7 +1,19 @@
+import {Price, Category, Property } from '../models/index.js'
 
 const init = async (req, res) => {
+
+    const [categories, prices] = await Promise.all([
+        Category.findAll({raw: true}),
+        Price.findAll({raw: true}),
+    ])
+
+    console.log(categories);
+
+
     res.render('init', {
-        page:'Init'
+        page:'Init',
+        categories,
+        prices
     })
 };
 
