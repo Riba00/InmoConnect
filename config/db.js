e9714136d747939a6f623d71c0acd72c64346db3
Riba00 <jordi.riba.ferre@gmail.com>
2024-07-11 18:17:27 +0200

    work with no docker

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import Sequelize from 'sequelize'
 import dotenv from 'dotenv'
 dotenv.config({path: '.env'})
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD ?? '', {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD ?? 'root', {
     host: process.env.DB_HOST,
     port: 3306,
     dialect: 'mysql',
--- public/js/map.js
+++ public/js/map.js
@@ -10,13 +10,13 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/js/showMap.js":
-/*!***************************!*\
-  !*** ./src/js/showMap.js ***!
-  \***************************/
+/***/ "./src/js/map.js":
+/*!***********************!*\
+  !*** ./src/js/map.js ***!
+  \***********************/
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n(function(){\n    const lat = document.querySelector('#lat').textContent\n    const lng = document.querySelector('#lng').textContent\n    const street = document.querySelector('#street').textContent\n    const title = document.querySelector('#title').textContent\n\n    const map = L.map('map').setView([lat, lng], 16)\n\n    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\n        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\n    }).addTo(map);\n\n    // Add pin\n    L.marker([lat, lng])\n        .addTo(map)\n        .bindPopup(street)\n})()\n\n//# sourceURL=webpack://bienesraices_mvc/./src/js/showMap.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n(function() {\n    const lat = document.querySelector('#lat').value || 40.6555447;\n    const lng = document.querySelector('#lng').value || 0.4683695;\n    const map = L.map('mapa').setView([lat, lng ], 16);\n\n    let marker;\n\n    const geocodeService = L.esri.Geocoding.geocodeService();\n    \n\n    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\n        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\n    }).addTo(map);\n\n    // PIN\n    marker = new L.marker([lat, lng], {\n        draggable: true,\n        autoPan: true\n    }).addTo(map)\n\n    // Detect marker movement\n    marker.on('moveend', function (e) {\n        marker = e.target\n        const position = marker.getLatLng();\n        map.panTo(new L.LatLng(position.lat, position.lng))\n\n        // Get the address of the marker\n        geocodeService.reverse().latlng(position, 13).run(function(error, result){\n            // console.log(result);\n            marker.bindPopup(result.address.LongLabel)\n\n            document.querySelector('.street').textContent = result?.address?.Address ?? '';\n            document.querySelector('#street').value = result?.address?.Address ?? '';\n            document.querySelector('#lat').value = result?.latlng?.lat ?? '';\n            document.querySelector('#lng').value = result?.latlng?.lng ?? '';\n        })\n    })\n\n})()\n\n//# sourceURL=webpack://inmoConnect/./src/js/map.js?");
 
 /***/ })
 
@@ -43,7 +43,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n(function(){\n    const lat =
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/js/showMap.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	__webpack_modules__["./src/js/map.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
